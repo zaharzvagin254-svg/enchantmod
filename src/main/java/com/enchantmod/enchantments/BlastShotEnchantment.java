bd281cf71868f5ce328f5c58eadb8baa822b4ff3
zaharzvagin254-svg <zaharzvagin254@gmail.com>
2026-03-14 14:59:51 +0300

    Update BlastShotEnchantment.java

--- src/main/java/com/enchantmod/enchantments/BlastShotEnchantment.java
+++ src/main/java/com/enchantmod/enchantments/BlastShotEnchantment.java
@@ -1,10 +1,11 @@
 package com.enchantmod.enchantments;
 
-import net.minecraft.tags.ItemTags;
 import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraft.world.item.enchantment.EnchantmentCategory;
 import net.minecraft.world.item.enchantment.Enchantments;
 import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.item.BowItem;
+import net.minecraft.world.item.CrossbowItem;
 import net.minecraft.world.item.ItemStack;
 
 public class BlastShotEnchantment extends Enchantment {
@@ -19,28 +20,22 @@ public class BlastShotEnchantment extends Enchantment {
 
     @Override
     public boolean canEnchant(ItemStack stack) {
-        return stack.is(ItemTags.BOWS);
+        return stack.getItem() instanceof BowItem || stack.getItem() instanceof CrossbowItem;
     }
 
     @Override
     public boolean canApplyAtEnchantingTable(ItemStack stack) {
-        return stack.is(ItemTags.BOWS);
+        return stack.getItem() instanceof BowItem || stack.getItem() instanceof CrossbowItem;
     }
 
     @Override
-    public int getMaxLevel() {
-        return 3;
-    }
+    public int getMaxLevel() { return 3; }
 
     @Override
-    public int getMinCost(int level) {
-        return 20 + (level - 1) * 10;
-    }
+    public int getMinCost(int level) { return 20 + (level - 1) * 10; }
 
     @Override
-    public int getMaxCost(int level) {
-        return getMinCost(level) + 50;
-    }
+    public int getMaxCost(int level) { return getMinCost(level) + 50; }
 
     @Override
     public boolean checkCompatibility(Enchantment other) {
@@ -51,12 +46,8 @@ public class BlastShotEnchantment extends Enchantment {
     }
 
     @Override
-    public boolean isTradeable() {
-        return true;
-    }
+    public boolean isTradeable() { return true; }
 
     @Override
-    public boolean isDiscoverable() {
-        return true;
-    }
+    public boolean isDiscoverable() { return true; }
 }
