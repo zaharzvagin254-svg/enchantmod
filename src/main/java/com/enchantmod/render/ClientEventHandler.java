1f8eb15b9ff034c268584eede368d70fe52c0d74
zaharzvagin254-svg <zaharzvagin254@gmail.com>
2026-03-15 02:44:05 +0300

    Update ClientEventHandler.java

--- src/main/java/com/enchantmod/render/ClientEventHandler.java
+++ src/main/java/com/enchantmod/render/ClientEventHandler.java
@@ -2,37 +2,45 @@ package com.enchantmod.render;
 
 import com.enchantmod.EnchantMod;
 import com.enchantmod.ModEffects;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.renderer.entity.EntityRenderer;
-import net.minecraft.client.renderer.entity.LivingEntityRenderer;
-import net.minecraft.world.entity.EntityType;
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.vertex.VertexConsumer;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.texture.OverlayTexture;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.client.event.EntityRenderersEvent;
 import net.minecraftforge.client.event.RenderLivingEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.registries.ForgeRegistries;
+import org.joml.Matrix3f;
+import org.joml.Matrix4f;
 
-@Mod.EventBusSubscriber(modid = EnchantMod.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
-public class ClientEventHandler {
+@Mod.EventBusSubscriber(modid = EnchantMod.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE, value = Dist.CLIENT)
+public class ClientForgeEvents {
 
+    
+    
     @SubscribeEvent
-    @SuppressWarnings({"unchecked", "rawtypes"})
-    public static void addLayers(EntityRenderersEvent.AddLayers event) {
-        for (String skin : event.getSkins()) {
-            Object renderer = event.getSkin(skin);
-            if (renderer instanceof LivingEntityRenderer r) {
-                r.addLayer(new BlueFireRenderLayer(r));
-            }
+    public static void onRenderLivingPre(RenderLivingEvent.Pre<?, ?> event) {
+        LivingEntity entity = event.getEntity();
+        if (entity.hasEffect(ModEffects.BLUE_HELLFIRE.get())) {
+            
+            
+            entity.getPersistentData().putInt("infernum_fire_ticks", entity.getRemainingFireTicks());
+            entity.setRemainingFireTicks(-1);
         }
-        for (EntityType<?> type : ForgeRegistries.ENTITY_TYPES.getValues()) {
-            try {
-                Object renderer = event.getRenderer((EntityType) type);
-                if (renderer instanceof LivingEntityRenderer r) {
-                    r.addLayer(new BlueFireRenderLayer(r));
-                }
-            } catch (Exception ignored) {}
+    }
+
+    
+    @SubscribeEvent
+    public static void onRenderLivingPost(RenderLivingEvent.Post<?, ?> event) {
+        LivingEntity entity = event.getEntity();
+        if (entity.hasEffect(ModEffects.BLUE_HELLFIRE.get())) {
+            int ticks = entity.getPersistentData().getInt("infernum_fire_ticks");
+            if (ticks > 0) {
+                entity.setRemainingFireTicks(ticks);
+            }
         }
     }
 }
