9b0c95dd1a631710d1ba173a4d7616a1e1014d3c
zaharzvagin254-svg <zaharzvagin254@gmail.com>
2026-03-15 00:32:47 +0300

    Update ClientEventHandler.java

--- src/main/java/com/enchantmod/render/ClientEventHandler.java
+++ src/main/java/com/enchantmod/render/ClientEventHandler.java
@@ -1,12 +1,14 @@
 package com.enchantmod.render;
 
 import com.enchantmod.EnchantMod;
+import net.minecraft.client.renderer.entity.EntityRenderer;
 import net.minecraft.client.renderer.entity.LivingEntityRenderer;
 import net.minecraft.world.entity.EntityType;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.EntityRenderersEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.registries.ForgeRegistries;
 
 @Mod.EventBusSubscriber(modid = EnchantMod.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
 public class ClientEventHandler {
@@ -14,43 +16,22 @@ public class ClientEventHandler {
     @SubscribeEvent
     @SuppressWarnings({"unchecked", "rawtypes"})
     public static void addLayers(EntityRenderersEvent.AddLayers event) {
+        // Player skins
         for (String skin : event.getSkins()) {
-            var renderer = event.getSkin(skin);
+            Object renderer = event.getSkin(skin);
             if (renderer instanceof LivingEntityRenderer r) {
-                r.addLayer(new BlueFireRenderLayer<>(r));
+                r.addLayer(new BlueFireRenderLayer(r));
             }
         }
 
-        addLayer(event, EntityType.ZOMBIE);
-        addLayer(event, EntityType.SKELETON);
-        addLayer(event, EntityType.CREEPER);
-        addLayer(event, EntityType.SPIDER);
-        addLayer(event, EntityType.CAVE_SPIDER);
-        addLayer(event, EntityType.ENDERMAN);
-        addLayer(event, EntityType.WITCH);
-        addLayer(event, EntityType.HUSK);
-        addLayer(event, EntityType.STRAY);
-        addLayer(event, EntityType.DROWNED);
-        addLayer(event, EntityType.PILLAGER);
-        addLayer(event, EntityType.VINDICATOR);
-        addLayer(event, EntityType.EVOKER);
-        addLayer(event, EntityType.RAVAGER);
-        addLayer(event, EntityType.BLAZE);
-        addLayer(event, EntityType.GHAST);
-        addLayer(event, EntityType.WITHER_SKELETON);
-        addLayer(event, EntityType.PIGLIN);
-        addLayer(event, EntityType.PIGLIN_BRUTE);
-        addLayer(event, EntityType.ZOMBIFIED_PIGLIN);
-        addLayer(event, EntityType.GUARDIAN);
-        addLayer(event, EntityType.ELDER_GUARDIAN);
-        addLayer(event, EntityType.WITHER);
-    }
-
-    @SuppressWarnings({"unchecked", "rawtypes"})
-    private static void addLayer(EntityRenderersEvent.AddLayers event, EntityType<?> type) {
-        var renderer = event.getRenderer((EntityType) type);
-        if (renderer instanceof LivingEntityRenderer r) {
-            r.addLayer(new BlueFireRenderLayer<>(r));
+        // All entity types from registry
+        for (EntityType<?> type : ForgeRegistries.ENTITY_TYPES.getValues()) {
+            try {
+                Object renderer = event.getRenderer((EntityType) type);
+                if (renderer instanceof LivingEntityRenderer r) {
+                    r.addLayer(new BlueFireRenderLayer(r));
+                }
+            } catch (Exception ignored) {}
         }
     }
 }
