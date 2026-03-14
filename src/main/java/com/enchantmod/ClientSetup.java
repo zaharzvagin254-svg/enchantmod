4b8560ce9140a736aeb09b548943298b9614df03
zaharzvagin254-svg <zaharzvagin254@gmail.com>
2026-03-14 14:48:59 +0300

    Update ClientSetup.java

--- src/main/java/com/enchantmod/ClientSetup.java
+++ src/main/java/com/enchantmod/ClientSetup.java
@@ -3,7 +3,6 @@ package com.enchantmod;
 import net.minecraft.ChatFormatting;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Items;
 import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraft.world.item.enchantment.EnchantmentHelper;
 import net.minecraftforge.api.distmarker.Dist;
@@ -19,22 +18,21 @@ public class ClientSetup {
     @SubscribeEvent
     public static void onItemTooltip(ItemTooltipEvent event) {
         ItemStack stack = event.getItemStack();
-        Map<Enchantment, Integer> enchants;
+        Map<Enchantment, Integer> enchants = EnchantmentHelper.getEnchantments(stack);
 
-        if (stack.is(Items.ENCHANTED_BOOK)) {
-            enchants = EnchantmentHelper.getEnchantments(stack);
-        } else {
-            enchants = EnchantmentHelper.getEnchantments(stack);
-        }
+        boolean addedBlastDesc = false;
+        boolean addedLeechDesc = false;
 
         for (Enchantment ench : enchants.keySet()) {
-            if (ench == ModEnchantments.BLAST_SHOT.get()) {
+            if (ench == ModEnchantments.BLAST_SHOT.get() && !addedBlastDesc) {
                 event.getToolTip().add(Component.translatable("enchantment.enchantmod.blast_shot.desc")
                     .withStyle(ChatFormatting.GRAY));
+                addedBlastDesc = true;
             }
-            if (ench == ModEnchantments.BLOOD_LEECH.get()) {
+            if (ench == ModEnchantments.BLOOD_LEECH.get() && !addedLeechDesc) {
                 event.getToolTip().add(Component.translatable("enchantment.enchantmod.blood_leech.desc")
                     .withStyle(ChatFormatting.GRAY));
+                addedLeechDesc = true;
             }
         }
     }
