91568d69345bab91f9c991b11a83733a178285f3
zaharzvagin254-svg <zaharzvagin254@gmail.com>
2026-03-15 02:36:47 +0300

    Create ClientForgeEvents.java

---
+++ src/main/java/com/enchantmod/render/ClientForgeEvents.java
@@ -0,0 +1,38 @@
+package com.enchantmod.render;
+
+import com.enchantmod.EnchantMod;
+import com.enchantmod.ModEffects;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.entity.EntityRenderer;
+import net.minecraft.client.renderer.entity.LivingEntityRenderer;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.EntityRenderersEvent;
+import net.minecraftforge.client.event.RenderLivingEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.registries.ForgeRegistries;
+
+@Mod.EventBusSubscriber(modid = EnchantMod.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+public class ClientEventHandler {
+
+    @SubscribeEvent
+    @SuppressWarnings({"unchecked", "rawtypes"})
+    public static void addLayers(EntityRenderersEvent.AddLayers event) {
+        for (String skin : event.getSkins()) {
+            Object renderer = event.getSkin(skin);
+            if (renderer instanceof LivingEntityRenderer r) {
+                r.addLayer(new BlueFireRenderLayer(r));
+            }
+        }
+        for (EntityType<?> type : ForgeRegistries.ENTITY_TYPES.getValues()) {
+            try {
+                Object renderer = event.getRenderer((EntityType) type);
+                if (renderer instanceof LivingEntityRenderer r) {
+                    r.addLayer(new BlueFireRenderLayer(r));
+                }
+            } catch (Exception ignored) {}
+        }
+    }
+}
