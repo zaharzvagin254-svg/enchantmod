8cdc6e465980918ff47838ba42dc28c28426c0cc
zaharzvagin254-svg <zaharzvagin254@gmail.com>
2026-03-14 14:54:34 +0300

    Update BlastShotEnchantment.java

--- src/main/java/com/enchantmod/enchantments/BlastShotEnchantment.java
+++ src/main/java/com/enchantmod/enchantments/BlastShotEnchantment.java
@@ -1,12 +1,11 @@
 package com.enchantmod.enchantments;
 
+import net.minecraft.tags.ItemTags;
 import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraft.world.item.enchantment.EnchantmentCategory;
 import net.minecraft.world.item.enchantment.Enchantments;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.BowItem;
-import net.minecraft.world.item.CrossbowItem;
 
 public class BlastShotEnchantment extends Enchantment {
 
@@ -20,28 +19,28 @@ public class BlastShotEnchantment extends Enchantment {
 
     @Override
     public boolean canEnchant(ItemStack stack) {
-        return stack.getItem() instanceof BowItem || stack.getItem() instanceof CrossbowItem;
+        return stack.is(ItemTags.BOWS);
     }
 
     @Override
     public boolean canApplyAtEnchantingTable(ItemStack stack) {
-        return stack.getItem() instanceof BowItem || stack.getItem() instanceof CrossbowItem;
-    }
-
-    // Запрещает применение книги через наковальню на неподходящие предметы
-    @Override
-    public boolean isAllowedOnBooks() {
-        return true;
+        return stack.is(ItemTags.BOWS);
     }
 
     @Override
-    public int getMaxLevel() { return 3; }
+    public int getMaxLevel() {
+        return 3;
+    }
 
     @Override
-    public int getMinCost(int level) { return 20 + (level - 1) * 10; }
+    public int getMinCost(int level) {
+        return 20 + (level - 1) * 10;
+    }
 
     @Override
-    public int getMaxCost(int level) { return getMinCost(level) + 50; }
+    public int getMaxCost(int level) {
+        return getMinCost(level) + 50;
+    }
 
     @Override
     public boolean checkCompatibility(Enchantment other) {
@@ -52,8 +51,12 @@ public class BlastShotEnchantment extends Enchantment {
     }
 
     @Override
-    public boolean isTradeable() { return true; }
+    public boolean isTradeable() {
+        return true;
+    }
 
     @Override
-    public boolean isDiscoverable() { return true; }
+    public boolean isDiscoverable() {
+        return true;
+    }
 }
