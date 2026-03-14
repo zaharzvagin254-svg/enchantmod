53a010b53d07d606cf3264a22f735144657b7e24
zaharzvagin254-svg <zaharzvagin254@gmail.com>
2026-03-15 00:15:18 +0300

    Update ClientEventHandler.java

--- src/main/java/com/enchantmod/render/ClientEventHandler.java
+++ src/main/java/com/enchantmod/render/ClientEventHandler.java
@@ -1,10 +1,11 @@
 package com.enchantmod.render;
 
 import com.enchantmod.EnchantMod;
-import net.minecraft.client.renderer.entity.EntityRenderer;
+import net.minecraft.client.model.EntityModel;
 import net.minecraft.client.renderer.entity.LivingEntityRenderer;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.EntityRenderersEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -15,25 +16,55 @@ public class ClientEventHandler {
 
     @SubscribeEvent
     public static void addLayers(EntityRenderersEvent.AddLayers event) {
-        // Добавляем слой ко всем living entity типам
-        for (EntityType<?> entityType : event.getEntityTypes()) {
-            EntityRenderer<?> renderer = event.getRenderer(entityType);
-            if (renderer instanceof LivingEntityRenderer<?, ?> livingRenderer) {
-                addBlueFireLayer(livingRenderer);
+        // Добавляем слой для игрока
+        for (String skin : event.getSkins()) {
+            var renderer = event.getSkin(skin);
+            if (renderer != null) {
+                addLayer(renderer);
             }
         }
-        // Также добавляем для игрока
-        for (String skin : event.getSkins()) {
-            var playerRenderer = event.getSkin(skin);
-            if (playerRenderer != null) {
-                addBlueFireLayer(playerRenderer);
-            }
+
+        // Добавляем слой для всех живых мобов
+        addLayerForType(event, EntityType.ZOMBIE);
+        addLayerForType(event, EntityType.SKELETON);
+        addLayerForType(event, EntityType.CREEPER);
+        addLayerForType(event, EntityType.SPIDER);
+        addLayerForType(event, EntityType.CAVE_SPIDER);
+        addLayerForType(event, EntityType.ENDERMAN);
+        addLayerForType(event, EntityType.WITCH);
+        addLayerForType(event, EntityType.HUSK);
+        addLayerForType(event, EntityType.STRAY);
+        addLayerForType(event, EntityType.DROWNED);
+        addLayerForType(event, EntityType.PILLAGER);
+        addLayerForType(event, EntityType.VINDICATOR);
+        addLayerForType(event, EntityType.EVOKER);
+        addLayerForType(event, EntityType.RAVAGER);
+        addLayerForType(event, EntityType.BLAZE);
+        addLayerForType(event, EntityType.GHAST);
+        addLayerForType(event, EntityType.WITHER_SKELETON);
+        addLayerForType(event, EntityType.PIGLIN);
+        addLayerForType(event, EntityType.PIGLIN_BRUTE);
+        addLayerForType(event, EntityType.ZOMBIFIED_PIGLIN);
+        addLayerForType(event, EntityType.HOGLIN);
+        addLayerForType(event, EntityType.ZOGLIN);
+        addLayerForType(event, EntityType.GUARDIAN);
+        addLayerForType(event, EntityType.ELDER_GUARDIAN);
+        addLayerForType(event, EntityType.WITHER);
+        addLayerForType(event, EntityType.ENDER_DRAGON);
+    }
+
+    @SuppressWarnings("unchecked")
+    private static <T extends LivingEntity, M extends EntityModel<T>>
+    void addLayerForType(EntityRenderersEvent.AddLayers event, EntityType<T> type) {
+        var renderer = event.getRenderer(type);
+        if (renderer instanceof LivingEntityRenderer<?, ?> livingRenderer) {
+            addLayer((LivingEntityRenderer<T, M>) livingRenderer);
         }
     }
 
     @SuppressWarnings("unchecked")
-    private static <T extends LivingEntity, M extends net.minecraft.client.model.EntityModel<T>>
-    void addBlueFireLayer(LivingEntityRenderer<T, M> renderer) {
+    private static <T extends LivingEntity, M extends EntityModel<T>>
+    void addLayer(LivingEntityRenderer<T, M> renderer) {
         renderer.addLayer(new BlueFireRenderLayer<>(renderer));
     }
 }
