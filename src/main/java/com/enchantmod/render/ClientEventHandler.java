5a6535af8eb0d400f7991d8d8fe0d1739c74c280
zaharzvagin254-svg <zaharzvagin254@gmail.com>
2026-03-15 00:26:41 +0300

    Update ClientEventHandler.java

--- src/main/java/com/enchantmod/render/ClientEventHandler.java
+++ src/main/java/com/enchantmod/render/ClientEventHandler.java
@@ -1,10 +1,8 @@
 package com.enchantmod.render;
 
 import com.enchantmod.EnchantMod;
-import net.minecraft.client.model.EntityModel;
 import net.minecraft.client.renderer.entity.LivingEntityRenderer;
 import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.entity.LivingEntity;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.EntityRenderersEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -14,49 +12,45 @@ import net.minecraftforge.fml.common.Mod;
 public class ClientEventHandler {
 
     @SubscribeEvent
+    @SuppressWarnings({"unchecked", "rawtypes"})
     public static void addLayers(EntityRenderersEvent.AddLayers event) {
         for (String skin : event.getSkins()) {
             var renderer = event.getSkin(skin);
-            if (renderer != null) {
-                addLayer(renderer);
+            if (renderer instanceof LivingEntityRenderer r) {
+                r.addLayer(new BlueFireRenderLayer<>(r));
             }
         }
 
-        addLayerForType(event, EntityType.ZOMBIE);
-        addLayerForType(event, EntityType.SKELETON);
-        addLayerForType(event, EntityType.CREEPER);
-        addLayerForType(event, EntityType.SPIDER);
-        addLayerForType(event, EntityType.CAVE_SPIDER);
-        addLayerForType(event, EntityType.ENDERMAN);
-        addLayerForType(event, EntityType.WITCH);
-        addLayerForType(event, EntityType.HUSK);
-        addLayerForType(event, EntityType.STRAY);
-        addLayerForType(event, EntityType.DROWNED);
-        addLayerForType(event, EntityType.PILLAGER);
-        addLayerForType(event, EntityType.VINDICATOR);
-        addLayerForType(event, EntityType.EVOKER);
-        addLayerForType(event, EntityType.RAVAGER);
-        addLayerForType(event, EntityType.BLAZE);
-        addLayerForType(event, EntityType.GHAST);
-        addLayerForType(event, EntityType.WITHER_SKELETON);
-        addLayerForType(event, EntityType.PIGLIN);
-        addLayerForType(event, EntityType.PIGLIN_BRUTE);
-        addLayerForType(event, EntityType.ZOMBIFIED_PIGLIN);
-        addLayerForType(event, EntityType.GUARDIAN);
-        addLayerForType(event, EntityType.ELDER_GUARDIAN);
-        addLayerForType(event, EntityType.WITHER);
+        addLayer(event, EntityType.ZOMBIE);
+        addLayer(event, EntityType.SKELETON);
+        addLayer(event, EntityType.CREEPER);
+        addLayer(event, EntityType.SPIDER);
+        addLayer(event, EntityType.CAVE_SPIDER);
+        addLayer(event, EntityType.ENDERMAN);
+        addLayer(event, EntityType.WITCH);
+        addLayer(event, EntityType.HUSK);
+        addLayer(event, EntityType.STRAY);
+        addLayer(event, EntityType.DROWNED);
+        addLayer(event, EntityType.PILLAGER);
+        addLayer(event, EntityType.VINDICATOR);
+        addLayer(event, EntityType.EVOKER);
+        addLayer(event, EntityType.RAVAGER);
+        addLayer(event, EntityType.BLAZE);
+        addLayer(event, EntityType.GHAST);
+        addLayer(event, EntityType.WITHER_SKELETON);
+        addLayer(event, EntityType.PIGLIN);
+        addLayer(event, EntityType.PIGLIN_BRUTE);
+        addLayer(event, EntityType.ZOMBIFIED_PIGLIN);
+        addLayer(event, EntityType.GUARDIAN);
+        addLayer(event, EntityType.ELDER_GUARDIAN);
+        addLayer(event, EntityType.WITHER);
     }
 
     @SuppressWarnings({"unchecked", "rawtypes"})
-    private static void addLayerForType(EntityRenderersEvent.AddLayers event, EntityType<?> type) {
-        var renderer = event.getRenderer(type);
-        if (renderer instanceof LivingEntityRenderer livingRenderer) {
-            addLayer(livingRenderer);
+    private static void addLayer(EntityRenderersEvent.AddLayers event, EntityType<?> type) {
+        var renderer = event.getRenderer((EntityType) type);
+        if (renderer instanceof LivingEntityRenderer r) {
+            r.addLayer(new BlueFireRenderLayer<>(r));
         }
-    }
-
-    @SuppressWarnings({"unchecked", "rawtypes"})
-    private static void addLayer(LivingEntityRenderer renderer) {
-        renderer.addLayer(new BlueFireRenderLayer<>(renderer));
     }
 }
