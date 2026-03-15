8ef91585b6b4f49f22da0bad57eb56d422868c07
zaharzvagin254-svg <zaharzvagin254@gmail.com>
2026-03-15 15:10:28 +0300

    Update BlueHellfireEffect.java

--- src/main/java/com/enchantmod/effect/BlueHellfireEffect.java
+++ src/main/java/com/enchantmod/effect/BlueHellfireEffect.java
@@ -12,13 +12,11 @@ public class BlueHellfireEffect extends MobEffect {
 
     @Override
     public void applyEffectTick(LivingEntity entity, int amplifier) {
-        // x2 damage compared to vanilla fire (vanilla onFire = 1 per second)
-        entity.hurt(entity.level().damageSources().onFire(), 2.0f);
+        entity.hurt(entity.level().damageSources().magic(), 1.0f);
     }
 
     @Override
     public boolean isDurationEffectTick(int duration, int amplifier) {
-        // Every 20 ticks = 1 second
-        return duration % 20 == 0;
+        return duration % 10 == 0;
     }
 }
