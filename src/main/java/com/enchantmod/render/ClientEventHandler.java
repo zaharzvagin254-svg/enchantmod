7a4d8d494e06bbe58f8aa03500a290d8f8c2c89e
zaharzvagin254-svg <zaharzvagin254@gmail.com>
2026-03-15 17:33:13 +0300

    Update ClientEventHandler.java

--- src/main/java/com/enchantmod/render/ClientEventHandler.java
+++ src/main/java/com/enchantmod/render/ClientEventHandler.java
@@ -1,24 +1,16 @@
 package com.enchantmod.render;
 
 import com.enchantmod.EnchantMod;
-import com.enchantmod.ModParticles;
-import com.enchantmod.particle.BlueSparkParticle;
 import net.minecraft.client.renderer.entity.LivingEntityRenderer;
 import net.minecraft.world.entity.EntityType;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.EntityRenderersEvent;
-import net.minecraftforge.client.event.RegisterParticleProvidersEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.registries.ForgeRegistries;
 
 @Mod.EventBusSubscriber(modid = EnchantMod.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
 public class ClientEventHandler {
-
-    @SubscribeEvent
-    public static void registerParticles(RegisterParticleProvidersEvent event) {
-        event.registerSpriteSet(ModParticles.BLUE_SPARK.get(), BlueSparkParticle.Provider::new);
-    }
 
     @SubscribeEvent
     @SuppressWarnings({"unchecked", "rawtypes"})
@@ -40,6 +32,5 @@ public class ClientEventHandler {
                 }
             } catch (Exception ignored) {}
         }
-        EnchantMod.LOGGER.info("[EnchantMod] BlueFireRenderLayer added to {} renderers", count);
     }
 }
