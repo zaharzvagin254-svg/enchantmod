3f73d2a3bc44f32ddebda3f7b30e3b9fcac57fe3
zaharzvagin254-svg <zaharzvagin254@gmail.com>
2026-03-15 03:12:19 +0300

    Update ClientForgeEvents.java

--- src/main/java/com/enchantmod/render/ClientForgeEvents.java
+++ src/main/java/com/enchantmod/render/ClientForgeEvents.java
@@ -2,45 +2,30 @@ package com.enchantmod.render;
 
 import com.enchantmod.EnchantMod;
 import com.enchantmod.ModEffects;
-import com.mojang.blaze3d.vertex.PoseStack;
-import com.mojang.blaze3d.vertex.VertexConsumer;
-import net.minecraft.client.renderer.MultiBufferSource;
-import net.minecraft.client.renderer.RenderType;
-import net.minecraft.client.renderer.texture.OverlayTexture;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.RenderLivingEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import org.joml.Matrix3f;
-import org.joml.Matrix4f;
 
 @Mod.EventBusSubscriber(modid = EnchantMod.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE, value = Dist.CLIENT)
 public class ClientForgeEvents {
 
-    
-    
     @SubscribeEvent
     public static void onRenderLivingPre(RenderLivingEvent.Pre<?, ?> event) {
         LivingEntity entity = event.getEntity();
         if (entity.hasEffect(ModEffects.BLUE_HELLFIRE.get())) {
-            
-            
-            entity.getPersistentData().putInt("infernum_fire_ticks", entity.getRemainingFireTicks());
+            entity.getPersistentData().putInt("saved_fire_ticks", entity.getRemainingFireTicks());
             entity.setRemainingFireTicks(-1);
         }
     }
 
-    
     @SubscribeEvent
     public static void onRenderLivingPost(RenderLivingEvent.Post<?, ?> event) {
         LivingEntity entity = event.getEntity();
         if (entity.hasEffect(ModEffects.BLUE_HELLFIRE.get())) {
-            int ticks = entity.getPersistentData().getInt("infernum_fire_ticks");
-            if (ticks > 0) {
-                entity.setRemainingFireTicks(ticks);
-            }
+            int ticks = entity.getPersistentData().getInt("saved_fire_ticks");
+            entity.setRemainingFireTicks(ticks);
         }
     }
 }
